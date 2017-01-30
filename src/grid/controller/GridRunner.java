b78d95d13ac031766f90e2cf59c64aad740b970c
GearShift24 <gearshift24@gmail.com>
2017-01-30 09:21:04 -0700

    mad3 doggo project

---
+++ src/grid/controller/GridRunner.java
@@ -0,0 +1,11 @@
+package grid.controller;
+
+public class GridRunner 
+{
+	public static void main (String [] args)
+	{
+		GridController doggoController = new GridController();
+		doggoController.start();
+	}
+	
+}
---
+++ src/grid/view/GridPanel.java
@@ -0,0 +1,37 @@
+package grid.view;
+
+import javax.swing.*;
+import grid.controller.GridController;
+
+public class GridPanel extends JPanel 
+{
+
+	private JTextField rowField;
+	private JTextField columnField;
+	private JtextField inputField;
+	private JLabel inputLabel;
+	private JLabel rowLabel;
+	private JLabel columnLabel;
+	private JButton submitButtonl;
+	private SpringLayout baseLayout;
+	private GridController baseController;
+	
+		
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
+	
+}
